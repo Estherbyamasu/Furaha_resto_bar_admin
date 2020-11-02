4ba4ca8adf40c50f9401714802a87b80ba9e4366
estherfuraha <estherbyamasufuraha@gmail.com>
2020-11-02 00:52:12 -0800

    update in route categories

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,14 @@ Auth::routes();
 Route::get('/', 'HomeController@index');
 Route::get('/', 'HomeController@index')->name('home');
 Route::resource('/Admin/users','Admin/UsersController');
+
+Route::get('categories','CategoriesController@index');
+Route::get('categories/create','CategoriesController@create');
+Route::post('categories','CategoriesController@store');
+Route::post('categorie','CategoriesController@storeprod');
+Route::get('categories/edit/{category}','CategoriesController@edit');
+
+Route::get('categories/show/{category}','CategoriesController@show');
+Route::put('categories/{category}','CategoriesController@update');
+
+Route::post('categories/destroy/{category}','CategoriesController@destroy');
