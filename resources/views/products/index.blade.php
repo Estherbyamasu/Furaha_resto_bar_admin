b7d7bf06483a0740b6101b4285e1efdb3a54dacd
estherfuraha <estherbyamasufuraha@gmail.com>
2020-11-02 03:04:27 -0800

    update in view/products

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -20,14 +20,59 @@
                 <div class="panel-body">
                     <div class="col-lg-12">
                     <div class="row">
-        <div class="col-lg-12">
-        <div class="modal-dialog modal-lg" role="document">
-             <div class="modal-content">
+       
 
-      <ul class="nav nav-tabs md-tabs tabs-2 light-blue darken-3" role="tablist">
-            <h1 class="page-header">L'ajout des Produits</h1>
-        </ul>
-             <div class="panel panel-default">
+    
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+ Inserer un nouveau produit
+</button>
+          
+            <table id="example1"  class="table table-bordered table-hover table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Category name</th>
+                        <th scope="col">Nom produit</th>
+                        <th scope="col">Prix</th>
+                        <th scope="col">Options</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($products as $product)
+                   
+                        <td>{{$product->id}}</td>
+                        <td>{{$product->cat_name}}</td>
+                        <td>{{$product->nom_produit}}</td>
+                        <td>{{$product->prix}}</td>
+                        <td>
+                            <a href="products/edit/{{$product->id}}" class="glyphicon glyphicon-edit   btn btn-primary">Edit</a>
+                            <a href="products/show/{{$product->id}}" class="glyphicon glyphicon-play btn btn-primary" >Voir les produits</a>
+                            <form action="products/destroy/{{$product->id}}" method="POST">
+                            @csrf
+                                <button type="submit" onclick="return confirm('Voulez vs vraiment supprimer ce produit ?')" class="glyphicon glyphicon-delite glyphicon-trash   btn btn-danger">Delete</button>
+                            </form>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <!--/.row-->
+   
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h2 class="modal-title" id="exampleModalCenterTitle">L'ajout des Produits</h2>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      <div class="panel panel-default">
              <div class="panel-body">
                     <div class="col-md-8">
            
@@ -72,7 +117,16 @@
                     @enderror
                 </div>
                 
-                <div class="row">
+               
+            </form>
+       
+      
+        </div>
+        </div>
+        </div>
+      </div>
+      <div class="row">
+      <div class="modal-footer">
             <div class="text-center mb-3 col-md-6">
               <button type="submit" class=" glyphicon glyphicon-plus btn  btn-primary btn-block btn-rounded z-depth-1">Save</button>
               </div>
@@ -80,53 +134,10 @@
               <button  type="reset"class="btn btn-outline-info waves-effect ml-auto" data-dismiss="modal">Reset</button>
               </div>
              </div>
-            </form>
-       
-      
-        </div>
-        </div>
-        </div>
-        </div>
+             </div>
     </div>
-    <div >
-         
-          </div>
-            <table id="example1" class="table table-bordered table-hover table-striped">
-                <thead>
-                    <tr>
-                        <th scope="col">ID</th>
-                        <th scope="col">Category name</th>
-                        <th scope="col">Nom produit</th>
-                        <th scope="col">Prix</th>
-                        <th scope="col">Options</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($products as $product)
-                    <tr classe="info"></tr>
-                    <tr classe="success"></tr>
-                    <tr classe="primary"></tr>
-                    <tr classe="warning">
-                        <td>{{$product->id}}</td>
-                        <td>{{$product->cat_name}}</td>
-                        <td>{{$product->nom_produit}}</td>
-                        <td>{{$product->prix}}</td>
-                        <td>
-                            <a href="products/edit/{{$product->id}}" class="glyphicon glyphicon-edit   btn btn-primary">Edit</a>
-                            <a href="products/show/{{$product->id}}" class="glyphicon glyphicon-play btn btn-primary" >Voir les produits</a>
-                            <form action="products/destroy/{{$product->id}}" method="POST">
-                            @csrf
-                                <button type="submit" onclick="return confirm('Voulez vs vraiment supprimer ce produit ?')" class="glyphicon glyphicon-delite glyphicon-trash   btn btn-danger">Delete</button>
-                            </form>
-                        </td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
-    </div>
-    <!--/.row-->
-
+  </div>
+</div>
 
     </div>
         </div>
