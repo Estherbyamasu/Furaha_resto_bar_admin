64327bfaa07da2d9b7d9454557ad8f8e4f74c145
estherfuraha <estherbyamasufuraha@gmail.com>
2020-11-02 01:02:53 -0800

    update data in view index categories

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -64,7 +64,7 @@
             </div><!-- /.panel-->
         </div><!-- /.panel-->
 
-                        <table class="table table-bordered table-hover table-striped">
+                        <table  id="example1" class="table table-bordered table-hover table-striped">
                             <thead>
                                 <tr>
                                     <th scope="col">Id</th>
