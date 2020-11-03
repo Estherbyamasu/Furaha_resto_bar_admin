a459150fbe258f092e2d46975792457881ce430e
estherfuraha <estherbyamasufuraha@gmail.com>
2020-11-03 00:47:07 -0800

    update index.blade.php views/serveurs

--- resources/views/serveurs/index.blade.php
+++ resources/views/serveurs/index.blade.php
@@ -19,76 +19,10 @@
             
                 <div class="panel-body">
                     <div class="col-lg-12">
-                    <div class="row">
-        <div class="col-lg-10">
-        <div class="modal-dialog modal-lg" role="document">
-             <div class="modal-content">
-
-      <ul class="nav nav-tabs md-tabs tabs-2 light-blue darken-3" role="tablist">
-            <h1 class="page-header">L'ajout des serveurs</h1>
-        </ul>
-             <div class="panel panel-default">
-             <div class="panel-body">
-                    <div class="col-md-8">
-            
-            <form action="{{url('serveurs')}}" method="POST">
-                @csrf
-                <div class="row">
-                <div class="col-md-6">
-                <div class="form-group">
-                    <label for="">nom serveur</label>
-                    <input type="text" name="nom_serveur" id="" class="form-control" class="@error('nom_serveur') is-danger @enderror" placeholder="" aria-describedby="helpId" required>
-                    @error('nom_serveur')
-                    <div class="alert alert-danger">{{$message}}</div>
-                    @enderror
-                </div>
-                </div>
-                <div class="col-md-6">
-                <div class="form-group">
-                    <label for="">prenom serveur</label>
-                    <input type="text" name="prenom_serveur" id="" class="form-control" class="@error('prenom_serveur') is-danger @enderror" placeholder="" aria-describedby="helpId" required>
-                    @error('prenom_serveur')
-                    <div class="alert alert-danger">{{$message}}</div>
-                    @enderror
-                </div>
-                </div>
-                </div>
-                <div class="row">
-                <div class="col-md-6">
-                <div class="form-group">
-                    <label for="">Adresse</label>
-                    <input type="text" name="address" id="" class="form-control" class="@error('address') is-danger @enderror" placeholder="" aria-describedby="helpId"required >
-                    @error('address')
-                    <div class="alert alert-danger">{{$message}}</div>
-                    @enderror
-                </div>
-                </div>
-                <div class="col-md-6">
-                <div class="form-group">
-                    <label for="">Telephone</label>
-                    <input type="text" name="tel" id="" class="form-control" class="@error('tel') is-danger @enderror" placeholder="" aria-describedby="helpId" required>
-                    @error('tel')
-                    <div class="alert alert-danger">{{$message}}</div>
-                    @enderror
-                </div>
-                </div>
-                </div>
-                 
-                <div class="row">
-            <div class="text-center mb-3 col-md-6">
-              <button type="submit" class=" glyphicon glyphicon-plus btn  btn-primary btn-block btn-rounded z-depth-1">Save</button>
-              </div>
-              <div class="text-center mb-3 col-md-6">
-              <button  type="reset"class="btn btn-outline-info waves-effect ml-auto" data-dismiss="modal">Reset</button>
-              </div>
-             </div>
-            </form>
-            </div>
-        </div>
-        </div>
-        </div>
-        </div>
-    </div>
+               
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalLong">
+ Inserer un Nouveau Caissier
+</button>
             <table id="example1" class="table table-bordered table-hover table-striped">
                 <thead>
                     <tr>
@@ -135,7 +69,82 @@
     <!--/.row-->
 
 
-   
+    
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h2 class="modal-title" id="exampleModalLongTitle">L'ajout des serveurs</h2>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      <div class="panel panel-default">
+             <div class="panel-body">
+                    <div class="col-md-8">
+            
+            <form action="{{url('serveurs')}}" method="POST">
+                @csrf
+                <div class="row">
+               
+                <div class="form-group">
+                    <label for="">nom serveur</label>
+                    <input type="text" name="nom_serveur" id="" class="form-control" class="@error('nom_serveur') is-danger @enderror" placeholder="" aria-describedby="helpId" required>
+                    @error('nom_serveur')
+                    <div class="alert alert-danger">{{$message}}</div>
+                    @enderror
+                </div>
+             
+                <div class="form-group">
+                    <label for="">prenom serveur</label>
+                    <input type="text" name="prenom_serveur" id="" class="form-control" class="@error('prenom_serveur') is-danger @enderror" placeholder="" aria-describedby="helpId" required>
+                    @error('prenom_serveur')
+                    <div class="alert alert-danger">{{$message}}</div>
+                    @enderror
+                </div>
+             
+                
+                <div class="row">
+                
+                <div class="form-group">
+                    <label for="">Adresse</label>
+                    <input type="text" name="address" id="" class="form-control" class="@error('address') is-danger @enderror" placeholder="" aria-describedby="helpId"required >
+                    @error('address')
+                    <div class="alert alert-danger">{{$message}}</div>
+                    @enderror
+                </div>
+               
+                
+                <div class="form-group">
+                    <label for="">Telephone</label>
+                    <input type="text" name="tel" id="" class="form-control" class="@error('tel') is-danger @enderror" placeholder="" aria-describedby="helpId" required>
+                    @error('tel')
+                    <div class="alert alert-danger">{{$message}}</div>
+                    @enderror
+                </div>
+              
+                <div class="modal-footer"> 
+                <div class="row">
+            <div class="text-center mb-3 col-md-6">
+              <button type="submit" class=" glyphicon glyphicon-plus btn  btn-primary btn-block btn-rounded z-depth-1">Save</button>
+              </div>
+              <div class="text-center mb-3 col-md-6">
+              <button  type="reset"class="btn btn-outline-info waves-effect ml-auto" data-dismiss="modal">Reset</button>
+              </div>
+              </div>
+             </div>
+            </form>
+            </div>
+        </div>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+</div>
    
 </div><!-- /.row -->
 @endsection()
