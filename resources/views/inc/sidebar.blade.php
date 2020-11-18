037051c8a0fa544829d16ec605c1540d06687004
estherfuraha <estherbyamasufuraha@gmail.com>
2020-11-17 23:34:22 -0800

    update sidebar

--- resources/views/inc/sidebar.blade.php
+++ resources/views/inc/sidebar.blade.php
@@ -85,9 +85,11 @@
 					<li><a class="" href="{{ url('detaillefactures')}}">
 						<span class="fa fa-bar-chart">&nbsp;</span> Detaillefactures
 					</a></li>
-					<li><a class="" href="{{ url('users')}}">
+					</ul>
+			</li>
+					<!-- <li><a class="" href="{{ url('users')}}">
 						<span class="fa fa-bar-chart">&nbsp;</span> utilisateur
-					</a></li>
+					</a></li> -->
 					<!-- <li><a class="" href="{{ url('users')}}">
 						<span class="fa fa-user">&nbsp;</span> utilisateur 
 						
@@ -98,9 +100,8 @@
 						<span class="fa fa-bar-chart">&nbsp;</span> utilisateur
 					</a></li>
 					@endif -->
-					</ul>
-			</li>
-			<li  class="active" ><a data-toggle="collapse" href="#sub-item-2">
+				
+			<!-- <li  class="active" ><a data-toggle="collapse" href="#sub-item-2">
 				<em class="fa fa-cog">&nbsp;</em> Statistics<span data-toggle="collapse" href="#sub-item-2" class="icon pull-right"><em class="fa fa-plus"></em></span>
 				</a>
 				  <ul class="children collapse" id="sub-item-2">
@@ -114,12 +115,12 @@
 						<span class="fa fa-users">&nbsp;</span> Serveurs
 					</a></li>
 					</ul>
-					<!-- @if(isset(Auth::user()->admin) && Auth::user()->admin == 'Admin')
+					 @if(isset(Auth::user()->admin) && Auth::user()->admin == 'Admin')
 					<li><a class="" href="{{ url('register')}}">
 						<span class="fa fa-bar-chart">&nbsp;</span> utilisateur
 					</a></li>
 					@endif -->
-			</li>
+			<!-- </li>  -->
 		
 		
 	
